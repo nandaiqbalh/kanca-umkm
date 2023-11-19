2e6ab2155b5a3beb128c798d5165a7e302beee97
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2023-11-19 09:24:38 +0700

    Set up pembeli bottom navigation bar

--- app/src/main/java/com/nandaiqbalh/kancaumkm/presentation/ui/home/pembeli/PembeliActivity.kt
+++ app/src/main/java/com/nandaiqbalh/kancaumkm/presentation/ui/home/pembeli/PembeliActivity.kt
@@ -2,11 +2,65 @@ package com.nandaiqbalh.kancaumkm.presentation.ui.home.pembeli
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.nandaiqbalh.kancaumkm.R
 
 class PembeliActivity : AppCompatActivity() {
+	private lateinit var navController: NavController
+	private lateinit var bottomNav: BottomNavigationView
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+		supportActionBar?.hide()
 		setContentView(R.layout.activity_pembeli)
+
+		val navHostFragment = supportFragmentManager
+			.findFragmentById(R.id.fragmentContainerViewHome) as NavHostFragment
+
+		navController = navHostFragment.navController
+
+		bottomNav = findViewById(R.id.bottom_navigation_user)
+
+		val appBarConfiguration = AppBarConfiguration.Builder(
+			R.id.pembeliBerandaFragment,
+			R.id.pembeliTokoPremiumFragment,
+			R.id.pembeliRiwayatFragment,
+			R.id.pembeliProfilFragment,
+
+			).build()
+
+		bottomNav.itemIconTintList = null
+
+		setupActionBarWithNavController(navController, appBarConfiguration)
+		bottomNav.setupWithNavController(navController)
+//		navController.addOnDestinationChangedListener { _, destination, _ ->
+//			when (destination.id) {
+//				R.id.action_provinceFragment_to_cityFragment -> {
+//					hideBottomNav(true)
+//				}
+//				R.id.action_cityFragment_to_provinceFragment -> {
+//					hideBottomNav(true)
+//				}
+//				else -> hideBottomNav(false)
+//			}
+//		}
+	}
+
+	private fun hideBottomNav(hide: Boolean) {
+		if (hide) {
+			bottomNav.visibility = View.GONE
+		} else {
+			bottomNav.visibility = View.VISIBLE
+		}
+	}
+
+	override fun onSupportNavigateUp(): Boolean {
+		return navController.navigateUp() || super.onSupportNavigateUp()
 	}
 }
---
+++ app/src/main/java/com/nandaiqbalh/kancaumkm/presentation/ui/home/pembeli/premium/PembeliTokoPremiumFragment.kt
@@ -0,0 +1,21 @@
+package com.nandaiqbalh.kancaumkm.presentation.ui.home.pembeli.premium
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nandaiqbalh.kancaumkm.R
+
+
+class PembeliTokoPremiumFragment : Fragment() {
+
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?,
+		savedInstanceState: Bundle?,
+	): View? {
+		// Inflate the layout for this fragment
+		return inflater.inflate(R.layout.fragment_pembeli_toko_premium, container, false)
+	}
+
+}
