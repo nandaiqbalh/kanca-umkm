171a2e1e20dd8943f71c5e5f4891eb08ec5162ee
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2024-01-01 00:34:06 +0700

    Fixing

--- app/src/main/java/com/nandaiqbalh/kancaumkm/presentation/ui/home/pembeli/premium/PembeliTokoPremiumFragment.kt
+++ app/src/main/java/com/nandaiqbalh/kancaumkm/presentation/ui/home/pembeli/premium/PembeliTokoPremiumFragment.kt
@@ -6,16 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.nandaiqbalh.kancaumkm.R
+import com.nandaiqbalh.kancaumkm.databinding.FragmentPembeliTokoPremiumBinding
 
 
 class PembeliTokoPremiumFragment : Fragment() {
 
+	private var _binding: FragmentPembeliTokoPremiumBinding? = null
+	private val binding get() = _binding!!
+
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
 		savedInstanceState: Bundle?,
-	): View? {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_pembeli_toko_premium, container, false)
+	): View{
+		_binding = FragmentPembeliTokoPremiumBinding.inflate(layoutInflater, container, false)
+		return binding.root
+	}
+
+	override fun onDestroy() {
+		super.onDestroy()
+		_binding = null
 	}
 
 }
